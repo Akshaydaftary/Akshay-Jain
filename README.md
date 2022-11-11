4721f28e40d8af0580e9f1b4d5eb1f31cec60a2f
Akshaydaftary <109431658+Akshaydaftary@users.noreply.github.com>
2022-11-11 18:05:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# C19-SA
+# Project 23
+
+Project Solution 23
--- sketch.js
+++ sketch.js
@@ -1,52 +1,58 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-let engine;
-let world;
-var ball;
-var ground;
-var wedge;
-var angle=60;
-var poly;
-var boxes=[];
+var engine, world;
+var canvas;
+var palyer, playerBase;
+var computer, computerBase;
+
 
 function setup() {
-  createCanvas(400,400);
+  canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
-  var ground_options={
-    isStatic:true
-  };
-  ground=Bodies.rectangle(100,300,400,20,ground_options);
-  World.add(world,ground);
-  box1 = new Box(200,100,50,50);
+
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+//create a player object from the Player class.
+  player= new Player(280,playerBase.body.position.y-150,50,180);
+
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
+
   
-
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
-}
-function mousePressed(){
-  boxes.push(
-    
-    
-    box1=new Box(mouseX,mouseY,50,50)
-    
-    
-  )
 }
 
-function draw() 
-{
-  background(51);
-  rect(ground.position.x,ground.position.y,400,10);
+function draw() {
+  background(189);
+
   Engine.update(engine);
- for(var i=0;i<boxes.length;i++) {
- boxes[i].show();}
-  
-  
-}
 
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+//call the display() function for the player object.
+  player.display();
+
+  computerBase.display();
+  computer.display();
+  
+
+}
